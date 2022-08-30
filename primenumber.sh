3349b7eac778f79f2d99c7169e39a8ba9dd157f6
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-08-30 08:45:58 +0530

    Added: forloop logic

---
+++ primenumber.sh
@@ -0,0 +1,18 @@
+read -p "enter a number:" num
+isprime=1
+for((i=2;i<=num/2;i++))
+do
+   if((num%i==0))
+   then
+   isprime=0
+   break
+   fi
+done
+    if((isprime==1))
+    then
+     echo "prime"
+    else
+     echo "Not prime"
+    fi
+
+
